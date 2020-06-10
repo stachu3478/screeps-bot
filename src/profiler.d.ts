2f1777eba4283ca96f0009412bfd61f19baf8b7a
stachu3478 <stachu3478@o2.pl>
2020-06-10 13:51:39 +0200

    Corrected screeps profiler typings

--- src/profiler.d.ts
+++ src/profiler.d.ts
@@ -2,6 +2,6 @@ declare type AnyFunction = (...args: any[]) => any
 
 declare module 'screeps-profiler' {
   export function enable(): void
-  export function registerFN(fn: AnyFunction, name?: string): AnyFunction
+  export function registerFN<T>(fn: T, name?: string): T
   export function wrap(fn: () => void): void
 }
